c762040645bac8c4aa44388214ecc5537a9d2632
ismail <ismailkocacan@gmail.com>
2018-03-15 23:54:54 +0300

    pass struct array. one important point fixed keyword. !!!

---
+++ PassStructArray/ClassLibrary1/Test.cs
@@ -0,0 +1,35 @@
+﻿using RGiesecke.DllExport;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+public struct Value
+{
+    public int FieldInt;
+    public double FieldDouble;
+}
+
+public class Test
+{
+    [DllExport("Post", CallingConvention.Cdecl)]
+    public static bool Post(ref Value elementFirst, ref Value elementLast)
+    {
+        unsafe
+        {
+            Value* current, last;
+            fixed (Value* tempFirst = &elementFirst, tempLast = &elementLast)
+            {
+                current = tempFirst;
+                last = tempLast;
+                current->FieldInt = 31;
+                while (current <= last)
+                {
+                    string value = string.Format("FInt: {0}, FDouble: {1}", 
+                                current->FieldInt, current->FieldDouble);
+                    MessageBox.Show(value, "Data");
+                    current++;
+                }
+            }
+        }
+        return true;
+    }
+}
