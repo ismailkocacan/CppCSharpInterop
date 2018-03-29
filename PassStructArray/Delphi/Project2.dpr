89a64fc27d8b1658f5492a85122153f7409f8436
ismail <ismailkocacan@gmail.com>
2018-03-30 00:34:02 +0300

    delphi client

---
+++ PassStructArray/Delphi/Project2.dpr
@@ -0,0 +1,61 @@
+{
+  ÝSMAÝL KOCACAN
+  30 Mart 2018 Cuma 00:29 PM
+}
+program Project2;
+
+{$APPTYPE CONSOLE}
+{$R *.res}
+
+uses
+  System.SysUtils,
+  Winapi.Windows,
+  Vcl.Dialogs;
+
+const
+  LibName = 'ClassLibrary1.dll';
+
+type
+
+  TValue = record
+    FieldInt: Integer;
+    FieldDouble: double;
+  end;
+
+  PValue = ^TValue;
+
+function Post(First, Last: PValue): Boolean; cdecl;
+  external LibName name 'Post';
+
+function GetList(ArrayLength: PINT): PValue; cdecl;
+  external LibName name 'GetList';
+
+var
+  AList: array of TValue;
+
+  AList2: PValue;
+  I: Integer;
+  ArrayLength: Integer;
+
+begin
+
+  SetLength(AList, 2);
+  AList[0].FieldInt := 33;
+  AList[0].FieldDouble := 33.33;
+
+  AList[1].FieldInt := 34;
+  AList[1].FieldDouble := 34.34;
+
+  Post(@AList[Low(AList)], @AList[High(AList)]);
+
+  AList2 := GetList(@ArrayLength);
+  if (ArrayLength = 0) then
+    exit;
+
+  for I := 0 to Pred(ArrayLength) do
+  begin
+    ShowMessage(AList2^.FieldInt.ToString());
+    Inc(AList2, sizeof(Pointer));
+  end;
+
+end.
