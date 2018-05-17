6120098b53d05bcaf5e09030869fb7d1097b8031
ismail <ismailkocacan@gmail.com>
2018-05-17 11:15:05 +0300

    call method via pointers

---
+++ CallMethodViaPointer/ConsoleApplication1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Collections.Generic;
+
+
+class Program
+{
+
+    public static void Procedure1()
+    {
+        Console.WriteLine("Procedure1");
+    }
+
+    public static void Procedure2()
+    {
+        Console.WriteLine("Procedure2");
+    }
+
+    static void Main(string[] args)
+    {
+        Queue<IntPtr> pointersQueue = new Queue<IntPtr>();
+
+        Type type = typeof(Program);
+        MethodInfo[] methods = type.GetMethods();
+        foreach (MethodInfo method in methods)
+        {
+            if (method.DeclaringType != type) continue;
+            pointersQueue.Enqueue(method.MethodHandle.GetFunctionPointer());
+        }
+
+        foreach (IntPtr intPtr in pointersQueue)
+        {
+            Marshal.GetDelegateForFunctionPointer(intPtr, typeof(Action)).DynamicInvoke(null);
+        }
+
+        while (pointersQueue.Count > 0)
+        {
+            Marshal.GetDelegateForFunctionPointer(pointersQueue.Dequeue(), typeof(Action)).DynamicInvoke(null);
+        }
+
+        Console.WriteLine(pointersQueue.Count.ToString());
+    }
+}
