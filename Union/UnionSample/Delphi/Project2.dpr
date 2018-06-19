54f671db44cda705a588253063e02deee02e10ec
ismail <ismailkocacan@gmail.com>
2018-06-19 22:24:23 +0300

    first implementation

--- Union/UnionSample/Delphi/Project2.dpr
+++ Union/UnionSample/Delphi/Project2.dpr
@@ -5,13 +5,57 @@ program Project2;
 {$R *.res}
 
 uses
-  System.SysUtils;
+  System.SysUtils,
+  Vcl.Dialogs;
 
+const
+  LibName = 'MyLib.dll';
+
+type
+
+ TDataType = (dtInteger, dtDouble);
+
+ {
+ 16 Byte
+ TUnionData = record
+   FieldInteger : Integer; 
+   FieldDouble : Double;
+ end;
+ }
+ 
+ // 8 Byte
+ TUnionData = record
+   case Integer of
+    0 : ( FieldInteger : Integer; );
+    1 : ( FieldDouble : Double; );
+ end;
+  
+ PUnionData = ^TUnionData;
+
+
+ TData = record
+   FieldInteger : Integer;
+   FieldDouble : Double;
+   DataType : TDataType;
+   UnionData : TUnionData;
+ end;
+
+ PData = ^TData;
+
+function CreateData:PData; cdecl; external LibName name 'CreateData';
+procedure RemoveData(DataPtr:PData); cdecl; external LibName name 'RemoveData';
+
+var
+  DataPtr : PData;
 begin
+  ShowMessage(sizeof(TUnionData).ToString());
+  
   try
-    { TODO -oUser -cConsole Main : Insert code here }
+    DataPtr := CreateData();
+
   except
     on E: Exception do
       Writeln(E.ClassName, ': ', E.Message);
   end;
+
 end.
